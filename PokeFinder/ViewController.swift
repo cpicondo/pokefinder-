7098da4896546f1be5fe0c7a9343f9af8f436c99
Christian Picondo <christianpicondo@gmail.com>
2017-04-18 23:19:37 +0800

    getting started files

--- PokeFinder/ViewController.swift
+++ PokeFinder/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func spotRandomPokemon(_ sender: UIButton) {
+    }
 
 }
 
