e9d1602fcaccab222fa17770ec83a776db5ede43
Christian Picondo <christianpicondo@gmail.com>
2017-04-20 00:25:32 +0800

    Firebase and GeoFire Installed

---
+++ PokeFinder/PokeFinder_Bridging_Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder_Bridging_Header.h
+//  PokeFinder
+//
+//  Created by christian Picondo on 19/04/2017.
+//  Copyright © 2017 cpicondo. All rights reserved.
+//
+
+#import "GeoFire.h"
--- PokeFinder/ViewController.swift
+++ PokeFinder/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import FirebaseDatabase
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
@@ -16,6 +17,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     let locationManager = CLLocationManager()
     var mapHasCenteredOnce = false
+    var geoFire: GeoFire!
+    var geoFireRef: FIRDatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +26,9 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         mapView.delegate = self
         mapView.userTrackingMode = MKUserTrackingMode.follow // the map follows the user's location
+        
+        geoFireRef = FIRDatabase.database().reference()
+        geoFire = GeoFire(firebaseRef: geoFireRef)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -30,6 +36,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationAuthStatus()
     }
     
+    //Ask user for location
     func locationAuthStatus() {
         
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
@@ -48,10 +55,11 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         }
     }
     
-    // function that center the user in mapview in 2 miles
+    // function that center the user in mapview
     func centerMapOnLocation(location: CLLocation) {
         
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        
         mapView.setRegion(coordinateRegion, animated: true)
     }
     
@@ -82,8 +90,43 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         return annotationView
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        mapView.isScrollEnabled = true
+        mapView.isZoomEnabled = true
+    }
+    
+    //Function calls when you see a pokemon
+    func createSighting(forLocation location:CLLocation, withPokemon pokeId: Int) {
+        
+        geoFire.setLocation(location, forKey: "\(pokeId)")
+    }
+    
+    //when the apps get the pokemon location, we need to show it on the map
+    func showSightingsOnMap(location: CLLocation) {
+        
+        let circleQuery = geoFire.query(at: location, withRadius: 2.5)
+        
+        _ = circleQuery?.observe(GFEventType.keyEntered, with: {(key, location) in
+        
+            
+            if let key = key, let location = location {
+                let anno = PokeAnnotation(coordinate: location.coordinate, pokemonNumber: Int(key)!)
+                self.mapView.addAnnotation(anno)
+                
+            }
+        })
+    }
+    
 
     @IBAction func spotRandomPokemon(_ sender: UIButton) {
+        
+        let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        
+        let rand = arc4random_uniform(151) + 1
+        createSighting(forLocation: loc, withPokemon: Int(rand))
+        
     }
 
 }
